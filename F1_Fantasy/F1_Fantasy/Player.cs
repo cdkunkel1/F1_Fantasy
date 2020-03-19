621c1495362ba0c25dc9e9ee2875c36a84b4b628
Cory Kunkel <cdkunkel1@crimson.ua.edu>
2020-03-19 13:48:03 -0400

    Finalize Start

--- F1_Fantasy/F1_Fantasy/Player.cs
+++ F1_Fantasy/F1_Fantasy/Player.cs
@@ -6,5 +6,32 @@ namespace F1_Fantasy
 {
     class Player
     {
+        private string name = ""; //Keeps track of the player's name
+        private int points = 0; //Keeps track of how many points the player has
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+        //This is a getter for the name
+        public string GetName()
+        {
+            return name;
+        }
+        //This is a getter for the points
+        public int GetPoints()
+        {
+            return points;
+        }
+        //This is a setter for the name
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        //This is a setter for the points
+        public void SetPoints(int points)
+        {
+            this.points = points;
+        }
     }
 }
