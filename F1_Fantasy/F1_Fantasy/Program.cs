fc30818918067d94188e1e2303538722f27332a3
cdkunkel1 <60272247+cdkunkel1@users.noreply.github.com>
2020-03-19 10:40:54 -0500

    Add files via upload

---
+++ F1_Fantasy/F1_Fantasy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace F1_Fantasy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Nathan");
+            Player player2 = new Player("Ben");
+            Player player3 = new Player("Cory");
+
+            DisplayPoints(player1.GetName(), player2.GetName(), player3.GetName());
+        }
+
+
+        public static void DisplayPoints(string player1, string player2, string player3)
+        {
+            Console.WriteLine("Here are the points currently held by each player:\n");
+            Console.WriteLine(player1 + ": " + currentPoints1);
+            Console.WriteLine
+        }
+    }
+}
