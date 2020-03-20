e01218a5b7bedcb311c12e4ac8689c03d3c01726
Cory Kunkel <cdkunkel1@crimson.ua.edu>
2020-03-20 13:30:36 -0400

    NEW: Connection to database
    Program now has a connection to the F1 database and can open and close that connection.

--- F1_Fantasy/F1_Fantasy/Program.cs
+++ F1_Fantasy/F1_Fantasy/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace F1_Fantasy
 {
@@ -6,13 +9,27 @@ namespace F1_Fantasy
     {
         static void Main(string[] args)
         {
+            //Declare Variables
+            string conString = @"Data Source = (LocalDB)\Formula_1; Initial Catalog = Formula_1; Integrated Security = True"; //This is the connection string for the F1 database
+            SqlConnection cnn = new SqlConnection(conString); //Create a SQL Connection object to connect to the F1 Database
+            OpenConnection(cnn); //Open the connection to the database
+
             Player player1 = new Player("Nathan");
             Player player2 = new Player("Ben");
             Player player3 = new Player("Cory");
 
             DisplayPoints(player1.GetName(), player2.GetName(), player3.GetName());
         }
-
+        //This method will open a connection to the SQL server
+        public static void OpenConnection(SqlConnection cnn)
+        {
+            cnn.Open();
+        }
+        //This method will close the connection to the SQL server
+        public static void CloseConnection(SqlConnection cnn)
+        {
+            cnn.Close();
+        }
 
         public static void DisplayPoints(string player1, string player2, string player3)
         {
