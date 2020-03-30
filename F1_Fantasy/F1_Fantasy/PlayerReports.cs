6def1214011945da1c3d2e874d049629cc454410
Cory Kunkel <cdkunkel1@crimson.ua.edu>
2020-03-30 19:59:08 -0400

    NEW: Questions Class and PlayerReports Class
    All of the questions are now counted and have names, and the amount of points from each question is now stored in the player objects. Next step is to display the points by question.

---
+++ F1_Fantasy/F1_Fantasy/PlayerReports.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1_Fantasy
+{
+    class PlayerReports
+    {
+        Player[] players;
+
+        public PlayerReports(Player[] temp)
+        {
+            this.players = temp;
+        }
+
+        public void PrintAllScores()
+        {
+            for (int x = 0; x < Player.GetCount(); x++)
+            {
+                Console.WriteLine(players[x].GetPointsByQuestion())
+            }
+        }
+    }
+}
